a0f1ecaf659b66e33d6dc7aae3d7b526b07ec304
Jolan ARNAUD <jolan.arnaud@gmail.com>
2025-01-16 11:45:19 +0100

    added types to class function variables

--- modules/sortable_class.py
+++ modules/sortable_class.py
@@ -1,14 +1,14 @@
 class Sortable:
-	def __init__(self, name, score, doubt):
+	def __init__(self, name:str, score:int, doubt:int):
 		self.name = name
 		self.score = score
 		self.doubt = doubt
 		
-	def rename(self, new_name):
+	def rename(self, new_name:str):
 		self.name = new_name
 	
-	def update_score(self, variation):
+	def update_score(self, variation:int):
 		self.score += variation
 	
-	def update_doubt(self, variation):
+	def update_doubt(self, variation:int):
 		self.doubt += variation
