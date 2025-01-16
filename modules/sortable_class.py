0e4b294d192fdc51b0e19c8e1fa92f98bd7dc130
Jolan ARNAUD <jolan.arnaud@gmail.com>
2025-01-16 12:15:30 +0100

    Added Image support for class

--- modules/sortable_class.py
+++ modules/sortable_class.py
@@ -1,9 +1,20 @@
+from PIL import Image,ImageTk
+
 class Sortable:
-	def __init__(self, name:str, score:int, doubt:int):
+	def __init__(self, name:str, score:int, doubt:int, image_path:str):
 		self.name = name
 		self.score = score
 		self.doubt = doubt
-		
+		self.image = self.load_image(image_path)
+	
+	def load_image(image_path:str) -> ImageTk:
+		try:
+			image = Image.open(image_path)
+			return ImageTk.PhotoImage(image)
+		except Exception as e:
+			print(f"Erreur de chargement de l'image : {e}")
+			return None
+
 	def rename(self, new_name:str):
 		self.name = new_name
 	
