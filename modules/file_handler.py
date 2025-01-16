1e2a01a08b727ab0f9dfd3f0259aeb1f60089d11
Jolan ARNAUD <jolan.arnaud@gmail.com>
2025-01-16 19:39:10 +0100

    Refacto and made _check_collection_validity private

--- modules/file_handler.py
+++ modules/file_handler.py
@@ -9,89 +9,9 @@ elif not os.path.exists("."+DATA_PATH+UNKNOWN_FILE):
 	print(f"Erreur : file_handler n'as pas trouvé {UNKNOWN_FILE}")
 
 
-def check_collection_validity(collection_path:str) -> int:
-	if not os.path.exists(collection_path):
-		print(f"Erreur : {collection_path} introuvable")
-		return 1
-	elif not os.path.exists(collection_path+SAVE_FILE):
-		print(f"Erreur : {SAVE_FILE} introuvable dans {collection_path}")
-		return 2
-	elif not os.path.exists(collection_path+IMAGE_PATH):
-		print(f"Erreur : {IMAGE_PATH} introuvable dans {collection_path}")
-		return 3
-	else:
-		return 0
-
-
-def generate_missing_png(collection:str):
-	collection_path = "."+DATA_PATH+"/"+collection
-	if check_collection_validity(collection_path) != 0:
-		return
-	
-	
-	with open(collection_path+SAVE_FILE,"r") as save_file:
-		name_list = []
-		for line in save_file.readlines()[1:]:
-			line_content_list = line.strip().split("\t")
-			name_list.append(line_content_list[0])
-	
-	for name in name_list:
-		if not os.path.exists(collection_path+IMAGE_PATH+"/"+name+".png"):
-			os.system("cp "+"."+DATA_PATH+UNKNOWN_FILE+" '"+collection_path+IMAGE_PATH+"/"+name+".png'")
-
-
-def clear_untracked_png(collection:str):
-	collection_path = "."+DATA_PATH+"/"+collection
-	if check_collection_validity(collection_path) != 0:
-		return
-
-	with open(collection_path+SAVE_FILE,"r") as save_file:
-		name_list = []
-		for line in save_file.readlines()[1:]:
-			line_content_list = line.strip().split("\t")
-			name_list.append(line_content_list[0])
-	
-	file_list = os.listdir(collection_path+IMAGE_PATH)
-	
-	for file in file_list:
-		if not file[:-4] in name_list:
-			os.remove(collection_path+IMAGE_PATH+"/"+file)
-
-
-def add_untracked_png(collection:str):
-	collection_path = "."+DATA_PATH+"/"+collection
-	if check_collection_validity(collection_path) != 0:
-		return
-
-	sortable_list = load(collection)
-	name_list = []
-	for sortable in sortable_list:
-		name_list.append(sortable.name)
-	
-	file_list = os.listdir(collection_path+IMAGE_PATH)
-	
-	with open(collection_path+SAVE_FILE,"a") as save_file:
-		for file in file_list:
-			if not file[:-4] in name_list:
-				save_file.write("\n"+file[:-4]+"\t"+str(DEFAULT_SCORE)+"\t"+str(DEFAULT_DOUBT))
-
-
-def generate_collection(collection:str):
-	collection_path = "."+DATA_PATH+"/"+collection
-	if not os.path.exists(collection_path):
-		os.mkdir(collection_path)
-	if not os.path.exists(collection_path+IMAGE_PATH):
-		os.mkdir(collection_path+IMAGE_PATH)
-	if not os.path.exists(collection_path+SAVE_FILE):
-		with open(collection_path+SAVE_FILE, "w") as save_file:
-			save_file.write("Name"+"\t"+"Score"+"\t"+"Doubt")
-
-	add_untracked_png(collection)
-
-
 def load(collection:str) -> list[Sortable]:
 	collection_path = "."+DATA_PATH+"/"+collection
-	if check_collection_validity(collection_path) != 0:
+	if _check_collection_validity(collection_path) != 0:
 		return
 	
 	with open(collection_path+SAVE_FILE,"r") as save_file:
@@ -111,9 +31,88 @@ def load(collection:str) -> list[Sortable]:
 
 def save(collection:str,sortable_list:list[Sortable]):
 	collection_path = "."+DATA_PATH+"/"+collection
-	if check_collection_validity(collection_path) != 0:
+	if _check_collection_validity(collection_path) != 0:
 		return
 	
 	with open(collection_path+SAVE_FILE, "w") as save_file:
 		save_file.write("Name"+"\t"+"Score"+"\t"+"Doubt"+"\n")
 		save_file.write('\n'.join(e.name+'\t'+str(e.score)+'\t'+str(e.doubt) for e in sortable_list))
+
+
+def generate_collection(collection:str):
+	collection_path = "."+DATA_PATH+"/"+collection
+	if not os.path.exists(collection_path):
+		os.mkdir(collection_path)
+	if not os.path.exists(collection_path+IMAGE_PATH):
+		os.mkdir(collection_path+IMAGE_PATH)
+	if not os.path.exists(collection_path+SAVE_FILE):
+		with open(collection_path+SAVE_FILE, "w") as save_file:
+			save_file.write("Name"+"\t"+"Score"+"\t"+"Doubt")
+
+	add_untracked_png(collection)
+
+
+def generate_missing_png(collection:str):
+	collection_path = "."+DATA_PATH+"/"+collection
+	if _check_collection_validity(collection_path) != 0:
+		return
+	
+	with open(collection_path+SAVE_FILE,"r") as save_file:
+		name_list = []
+		for line in save_file.readlines()[1:]:
+			line_content_list = line.strip().split("\t")
+			name_list.append(line_content_list[0])
+	
+	for name in name_list:
+		if not os.path.exists(collection_path+IMAGE_PATH+"/"+name+".png"):
+			os.system("cp "+"."+DATA_PATH+UNKNOWN_FILE+" '"+collection_path+IMAGE_PATH+"/"+name+".png'")
+
+
+def add_untracked_png(collection:str):
+	collection_path = "."+DATA_PATH+"/"+collection
+	if _check_collection_validity(collection_path) != 0:
+		return
+
+	sortable_list = load(collection)
+	name_list = []
+	for sortable in sortable_list:
+		name_list.append(sortable.name)
+	
+	file_list = os.listdir(collection_path+IMAGE_PATH)
+	
+	with open(collection_path+SAVE_FILE,"a") as save_file:
+		for file in file_list:
+			if not file[:-4] in name_list:
+				save_file.write("\n"+file[:-4]+"\t"+str(DEFAULT_SCORE)+"\t"+str(DEFAULT_DOUBT))
+
+
+def clear_untracked_png(collection:str):
+	collection_path = "."+DATA_PATH+"/"+collection
+	if _check_collection_validity(collection_path) != 0:
+		return
+
+	with open(collection_path+SAVE_FILE,"r") as save_file:
+		name_list = []
+		for line in save_file.readlines()[1:]:
+			line_content_list = line.strip().split("\t")
+			name_list.append(line_content_list[0])
+	
+	file_list = os.listdir(collection_path+IMAGE_PATH)
+	
+	for file in file_list:
+		if not file[:-4] in name_list:
+			os.remove(collection_path+IMAGE_PATH+"/"+file)
+
+
+def _check_collection_validity(collection_path:str) -> int:
+	if not os.path.exists(collection_path):
+		print(f"Erreur : {collection_path} introuvable")
+		return 1
+	elif not os.path.exists(collection_path+SAVE_FILE):
+		print(f"Erreur : {SAVE_FILE} introuvable dans {collection_path}")
+		return 2
+	elif not os.path.exists(collection_path+IMAGE_PATH):
+		print(f"Erreur : {IMAGE_PATH} introuvable dans {collection_path}")
+		return 3
+	else:
+		return 0
