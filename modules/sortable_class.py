907d5c1523d85e7d3ad8a4f483581c65e995faf4
Jolan ARNAUD <jolan.arnaud@gmail.com>
2025-01-17 02:31:36 +0100

    add clamping to doubt value

--- modules/sortable_class.py
+++ modules/sortable_class.py
@@ -1,3 +1,5 @@
+from config import *
+
 class Sortable:
 	def __init__(self, name:str, score:int, doubt:int, image_path:str):
 		self.name = name
@@ -10,7 +12,7 @@ class Sortable:
 	def update(self):
 		self.score += self.score_modification
 		self.score_modification = 0
-		self.doubt += self.doubt_modification
+		self.doubt = min(max(self.doubt + self.doubt_modification,MINIMUM_DOUBT),DEFAULT_DOUBT)
 		self.doubt_modification = 0
 
 	def display(self,show_image_path:bool=False):
