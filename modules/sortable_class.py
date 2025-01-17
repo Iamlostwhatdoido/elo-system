3e286d9e388cb03cd8a501703db1c8f4eeab10a8
Jolan ARNAUD <jolan.arnaud@gmail.com>
2025-01-17 02:14:02 +0100

    Deleted useless methods and added memory/update system

--- modules/sortable_class.py
+++ modules/sortable_class.py
@@ -2,29 +2,20 @@ class Sortable:
 	def __init__(self, name:str, score:int, doubt:int, image_path:str):
 		self.name = name
 		self.score = score
+		self.score_modification = 0
 		self.doubt = doubt
+		self.doubt_modification = 0
 		self.image_path = image_path
+
+	def update(self):
+		self.score += self.score_modification
+		self.score_modification = 0
+		self.doubt += self.doubt_modification
+		self.doubt_modification = 0
 
 	def print(self,show_image_path:bool=False):
 		if not show_image_path:
-			print(f"{self.name} [{self.score} ~{self.doubt}] ")
+			print(f"{self.name} [{self.score} {self.score_modification:+}] ~{self.doubt} {self.doubt_modification:+}")
 		else:
 			print(f"{self.name} [{self.score} ~{self.doubt}]  ({self.image_path})")
-
-	def rename(self, new_name:str):
-		self.name = new_name
 	
-	def update_score(self, variation:int):
-		self.score += variation
-	
-	def set_score(self, new_score:int):
-		self.score = new_score
-	
-	def update_doubt(self, variation:int):
-		self.doubt += variation
-	
-	def set_doubt(self, new_doubt:int):
-		self.doubt = new_doubt
-	
-	def change_image(self, new_path:str):
-		self.image_path = new_path
