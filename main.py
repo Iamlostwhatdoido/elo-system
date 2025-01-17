f1147b3d6ebea6eda57b391068a47f84bb3e576d
Jolan ARNAUD <jolan.arnaud@gmail.com>
2025-01-17 19:15:19 +0100

    setup basic main test

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import ui
+from modules import file_handler as fh, match_result_engine as mre
+from modules.sortable_class import Sortable
 
-def run():
+def test_ui():
 	ui.start_ui()
+
+def basic_duel():
+	sortable_list = fh.load('test')
+	chosen_two = sortable_list[35:37]
+
+	for sortable in chosen_two:
+		sortable.display()
+	
+	mre.resolve_match([chosen_two[0]],[chosen_two[1]])
+
+	for sortable in chosen_two:
+		sortable.display()
+
 
 
 if __name__ == "__main__":
-	run()
+	basic_duel()
