ee6285939cdbf168ec5b339dc3aad846ba48c29c
Jolan ARNAUD <jolan.arnaud@gmail.com>
2025-01-17 13:01:33 +0100

    created basic proto

--- ui.py
+++ ui.py
@@ -20,7 +20,13 @@ class Window(customtkinter.CTk):
 		test_sortable = self.controller.pick_random(1)[0]
 
 		button = self.create_sortable_button(test_sortable, False)
-		button.pack(padx=20, pady=20)
+		button.grid(row=1, column=0, padx=20, pady=20, sticky="ew")
+
+
+		my_image = customtkinter.CTkImage(light_image=Image.open(test_sortable.image_path),
+                                  size=(300, 300))
+		image_label = customtkinter.CTkLabel(self, image=my_image, text="")
+		image_label.grid(row=0, column=0, padx=20, pady=20, sticky="ew")
 
 	def button_callback(self):
 		print("button clicked")
@@ -30,7 +36,7 @@ class Window(customtkinter.CTk):
 		my_image = customtkinter.CTkImage(light_image=Image.open(sortable.image_path),
                                   size=(30, 30))
 		
-		button = customtkinter.CTkButton(self, text=sortable.name, image = my_image, command=self.button_callback)
+		button = customtkinter.CTkButton(self, text=sortable.name, image = my_image, compound=customtkinter.TOP, command=self.button_callback)
 		return button
 
 
