cd69657cdb6ec551c452ef384bafb0461bad14e6
Jolan ARNAUD <jolan.arnaud@gmail.com>
2025-01-17 02:56:06 +0100

    cleared up the script

--- main.py
+++ main.py
@@ -1,20 +1,9 @@
 from modules import file_handler as fh, match_result_engine as mre
 from modules.sortable_class import Sortable
 
-test_list = fh.load("test")
+def run():
+	pass
 
-winner_list = test_list[2:]
-print(f"\n - Winners : ")
-for test in winner_list:
-	test.display()
 
-loser_list = test_list[:2]
-print(f"\n - Losers : ")
-for test in loser_list:
-	test.display()
-
-mre.resolve_match(winner_list,loser_list)
-
-print(f"\n - Updated : ")
-for test in test_list:
-	test.display(True)
+if __name__ == "__main__":
+	run()
