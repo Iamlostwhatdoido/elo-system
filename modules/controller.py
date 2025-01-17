9e77cd02decc05b3540e413db9acf38d2d50f2f5
Jolan ARNAUD <jolan.arnaud@gmail.com>
2025-01-17 10:25:10 +0100

    initialized controller class

--- modules/controller.py
+++ modules/controller.py
@@ -1,6 +1,11 @@
+from config import *
 from .sortable_class import Sortable
+from modules import file_handler as fh, match_result_engine as mre
 
 
 
-
+class Controller:
+	def __init__(self):
+		self.loaded_sortables : list[Sortable] = []
+		self.current_collection : str = None
 
