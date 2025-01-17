abc1e71016694ca7042dc99c9e0f8e0868edb8a9
Jolan ARNAUD <jolan.arnaud@gmail.com>
2025-01-17 18:11:22 +0100

    refaction

--- ui.py
+++ ui.py
@@ -6,6 +6,54 @@ from PIL import ImageTk,Image
 import customtkinter
 
 
+def start_ui():
+	window = Window()
+	window.mainloop()
+
+
+class Window(customtkinter.CTk):
+	def __init__(self):
+		super().__init__()
+
+		self.controller = Controller()
+		self.title("Elo Calculator")
+		self.geometry("800x600")
+		self.grid_columnconfigure((0, 1, 2), weight=1)
+
+
+		self.controller.set_collection("test")
+		self.controller.load_collection()
+		self.test_sortable = self.controller.pick_random(1)[0]
+
+		self.collection_frame = CollectionFrame(master=self)
+		self.collection_frame.grid(row=0, column=0, padx=0, pady=0, sticky="ew", columnspan=3)
+
+
+	def button_callback(self):
+		print("button clicked")
+		self.big_image_label.grid_forget()
+
+
+	def create_sortable_button(self, sortable:Sortable, toggle:bool):
+		my_image = customtkinter.CTkImage(light_image=Image.open(sortable.image_path),
+								  size=(30, 30))
+		
+		button = customtkinter.CTkButton(self, text=sortable.name, image = my_image, compound=customtkinter.TOP, command=self.button_callback)
+		return button
+
+
+	def submit_match(self):
+		winner = self.winner_entry.get()
+		loser = self.loser_entry.get()
+		try:
+			relevance = float(self.relevance_entry.get())
+		except ValueError:
+			self.display_error("Relevance must be a number.")
+			return
+
+		self.controller.add_match(winner, loser, relevance)
+
+
 class CollectionFrame(customtkinter.CTkFrame):
 	def __init__(self, master, **kwargs):
 		super().__init__(master, fg_color='transparent', **kwargs)
@@ -55,52 +103,3 @@ class CollectionFrame(customtkinter.CTkFrame):
 	def save_event(self):
 		pass
 
-
-
-
-class Window(customtkinter.CTk):
-	def __init__(self):
-		super().__init__()
-
-		self.controller = Controller()
-		self.title("Elo Calculator")
-		self.geometry("800x600")
-		self.grid_columnconfigure((0, 1, 2), weight=1)
-
-
-		self.controller.set_collection("test")
-		self.controller.load_collection()
-		self.test_sortable = self.controller.pick_random(1)[0]
-
-		self.collection_frame = CollectionFrame(master=self)
-		self.collection_frame.grid(row=0, column=0, padx=0, pady=0, sticky="ew", columnspan=3)
-
-
-	def button_callback(self):
-		print("button clicked")
-		self.big_image_label.grid_forget()
-
-
-	def create_sortable_button(self, sortable:Sortable, toggle:bool):
-		my_image = customtkinter.CTkImage(light_image=Image.open(sortable.image_path),
-								  size=(30, 30))
-		
-		button = customtkinter.CTkButton(self, text=sortable.name, image = my_image, compound=customtkinter.TOP, command=self.button_callback)
-		return button
-
-
-	def submit_match(self):
-		winner = self.winner_entry.get()
-		loser = self.loser_entry.get()
-		try:
-			relevance = float(self.relevance_entry.get())
-		except ValueError:
-			self.display_error("Relevance must be a number.")
-			return
-
-		self.controller.add_match(winner, loser, relevance)
-
-
-def start_ui():
-	window = Window()
-	window.mainloop()
