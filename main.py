f1c93394feb7f62511971c5dcd68cec6ed99f176
Jolan ARNAUD <jolan.arnaud@gmail.com>
2025-01-19 18:48:33 +0100

    Finished doubt implementation

--- main.py
+++ main.py
@@ -1,22 +1,34 @@
 import ui
+import random
+
 from modules import file_handler as fh, match_result_engine as mre
 from modules.sortable_class import Sortable
 
-def test_ui():
+def launch():
 	ui.start_ui()
 
-def basic_duel():
-	sortable_list = fh.load('test')
-	chosen_two = sortable_list[35:37]
-
-	for sortable in chosen_two:
-		sortable.display()
+def calibration_test():
 	
-	mre.resolve_match([chosen_two[0]],[chosen_two[1]])
+	strong_unclear 	= lambda : Sortable("Strong & Unclear",	300,	400,"")
+	mid_unclear 	= lambda : Sortable("Mid & Unclear",		0,		400,"")
+	weak_unclear 	= lambda : Sortable("Weak & Unclear",	-300,	400,"")
+	strong_known 	= lambda : Sortable("Strong & Known",	300,	200,"")
+	mid_known 		= lambda : Sortable("Mid & Known",		250,		200,"")
+	weak_known 		= lambda : Sortable("Weak & Known",		-300,	200,"")
+	strong_precise 	= lambda : Sortable("Strong & Precise",	300,	50,"")
+	mid_precise 	= lambda : Sortable("Mid & Precise",		0,		50,"")
+	weak_precise 	= lambda : Sortable("Weak & Precise",	-300,	50,"")
 
-	for sortable in chosen_two:
-		sortable.display()
+	roster = [strong_unclear, mid_unclear,weak_unclear,strong_known,mid_known,weak_known,strong_precise,mid_precise,weak_precise]
 
+	# for _ in range(10):
+	# 	print("\n\n NEW MATCH !")
+	# 	sample = random.sample(roster,2)
+	# 	mre.resolve_match([sample[0]()],[sample[1]()],True)
+
+	mre.resolve_match([weak_precise()],[weak_unclear()],True)
+	mre.resolve_match([weak_precise()],[weak_known()],True)
+	mre.resolve_match([weak_precise()],[weak_precise()],True)
 
 if __name__ == "__main__":
-	test_ui()
+	launch()
--- modules/match_result_engine.py
+++ modules/match_result_engine.py
@@ -5,20 +5,40 @@ from .sortable_class import Sortable
 CORRECTION = np.log(EXPECTANCY_BASE)/STANDARD_GAP
 
 
-def resolve_match(winner_list:list[Sortable],loser_list:list[Sortable]) -> list[Sortable]:
+def resolve_match(winner_list:list[Sortable],loser_list:list[Sortable],verbose=False) -> list[Sortable]:
 	for winner in winner_list:
+		opponents = []
+		results = []
+
 		for opponent in loser_list:
-			_duel_calculation(winner,opponent,1)
+			opponents.append(opponent)
+			results.append(1)
+			
 		for opponent in winner_list:
 			if opponent == winner : continue
-			_duel_calculation(winner,opponent,0.5)
+			opponents.append(opponent)
+			results.append(0.5)
+
+		_duel_calculation(winner,opponents,results,verbose)
+		if verbose:
+			winner.display()
 	
 	for loser in loser_list:
+		opponents = []
+		results = []
+		
 		for opponent in loser_list:
 			if opponent == loser : continue
-			_duel_calculation(loser,opponent,0.5)
+			opponents.append(opponent)
+			results.append(0.5)
+
 		for opponent in winner_list:
-			_duel_calculation(loser,opponent,0)
+			opponents.append(opponent)
+			results.append(0)
+
+		_duel_calculation(loser,opponents,results,verbose)
+		if verbose:
+			loser.display()
 	
 	for winner in winner_list:
 		winner.update()
@@ -26,18 +46,49 @@ def resolve_match(winner_list:list[Sortable],loser_list:list[Sortable]) -> list[
 		loser.update()
 
 
-def _duel_calculation(subject:Sortable,opponent:Sortable, result:float):
-	opponent_pertinence = _pertinence(opponent.doubt)
-	subject_expectancy = _expectancy(subject.score, opponent.score, opponent_pertinence)
-	score_points = round(subject.doubt * opponent_pertinence * (result-subject_expectancy))
-	subject.score_modification += score_points
+def _duel_calculation(subject:Sortable,opponents:list[Sortable], results:list[float],verbose=False):
+
+	if verbose:
+		print(f'\n\nSubject = {subject.name}')
+
+	match_surprise = 0
+	match_informativity = 0
+	for i, opponent in enumerate(opponents):
+		opponent_pertinence = _pertinence(opponent.doubt)
+		subject_expectancy = _expectancy(subject.score, opponent.score, opponent_pertinence)
+
+		if verbose:
+			print(f'\n - VS = {opponent.name} (result : {results[i]})')
+			print(f'    pertin = {opponent_pertinence}')
+			print(f'    expect = {subject_expectancy}')
+
+
+		match_surprise += _surprise(subject_expectancy,results[i])
+		match_informativity += (opponent_pertinence**2)*subject_expectancy*(1-subject_expectancy)
+	
+	match_informativity *= CORRECTION**2
+
+	if verbose:
+		print(f'\n Session result :')
+		print(f'    surprise = {match_surprise}')
+		print(f'    inform = {match_informativity}')
+
+	score_points = 0
+	for i, opponent in enumerate(opponents):
+		score_points = _pertinence(opponent.doubt) * (results[i]-subject_expectancy)
+
+	new_doubt = _new_doubt(subject.doubt,match_surprise,match_informativity)
+	score_points *= new_doubt
+	doubt_points = new_doubt - subject.doubt
+
+	subject.score_modification += round(score_points)
+	subject.doubt_modification += round(doubt_points)
 
 
 def _pertinence(doubt:int) -> float:
 	return 1/np.sqrt(
 		1 + 3*( CORRECTION*doubt/np.pi )**2
 		)
-
 
 def _expectancy(subject_score:int, opponent_score:int,opponent_pertinence:float) -> float:
 	return 1 / (
@@ -48,3 +99,16 @@ def _expectancy(subject_score:int, opponent_score:int,opponent_pertinence:float)
 			)
 		)
 
+
+def _surprise(expectancy,result):
+	if abs(result-expectancy) < 0.25:
+		return 0
+	else:
+		return ((abs(result-expectancy) - 0.25) / 0.75)**2
+
+
+def _new_doubt(doubt,match_surprise,match_informativity):
+	out_squared = doubt**2
+	out_squared += SURPRISE_FACTOR*match_surprise
+	out_squared = 1/ (1/out_squared + match_informativity)
+	return np.sqrt(out_squared) 
