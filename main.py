669d47e966659b25f6fb98e293281d5e2730867f
Jolan ARNAUD <jolan.arnaud@gmail.com>
2025-01-19 18:50:00 +0100

    refacto

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ def launch():
 def calibration_test():
 	
 	strong_unclear 	= lambda : Sortable("Strong & Unclear",	300,	400,"")
-	mid_unclear 	= lambda : Sortable("Mid & Unclear",		0,		400,"")
+	mid_unclear 	= lambda : Sortable("Mid & Unclear",	0,		400,"")
 	weak_unclear 	= lambda : Sortable("Weak & Unclear",	-300,	400,"")
 	strong_known 	= lambda : Sortable("Strong & Known",	300,	200,"")
 	mid_known 		= lambda : Sortable("Mid & Known",		250,		200,"")
 	weak_known 		= lambda : Sortable("Weak & Known",		-300,	200,"")
 	strong_precise 	= lambda : Sortable("Strong & Precise",	300,	50,"")
-	mid_precise 	= lambda : Sortable("Mid & Precise",		0,		50,"")
+	mid_precise 	= lambda : Sortable("Mid & Precise",	0,		50,"")
 	weak_precise 	= lambda : Sortable("Weak & Precise",	-300,	50,"")
 
 	roster = [strong_unclear, mid_unclear,weak_unclear,strong_known,mid_known,weak_known,strong_precise,mid_precise,weak_precise]
