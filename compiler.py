b83c63ac59afc5831b52c412edb0520107e7a800
Jolan ARNAUD <jolan.arnaud@gmail.com>
2025-01-26 14:51:58 +0100

    Created a compiler tool using PyInstaller

---
+++ compiler.py
@@ -0,0 +1,22 @@
+import os
+
+EXE_NAME = "Elo Sorter"
+VERSION = "0.1.0"
+
+MAIN_SCRIPT = "main"
+ICON_FILE = "icon.ico"
+
+if os.path.exists(f"./releases/{EXE_NAME} v{VERSION}.exe"):
+	print(f"ERROR : Version {VERSION} already exists.")
+else:
+	command = f"python -m PyInstaller {MAIN_SCRIPT}.py " \
+		f"--onefile " \
+		f"--icon {ICON_FILE} " \
+		f"--distpath releases " \
+		f'-n "{EXE_NAME} v{VERSION}"'
+
+	print(command)
+	os.system(command)
+
+	os.system("rm -rf build")
+	os.remove(f"{EXE_NAME} v{VERSION}.spec")
